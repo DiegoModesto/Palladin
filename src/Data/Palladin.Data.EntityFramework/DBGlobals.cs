1ccb6052a0868f70846c20559be2a20e003b6791
Diego Modesto <diegosanches89@gmail.com>
2019-09-23 12:15:57 -0300

    Adicionado docker compose para utilizaçãop de containers

--- src/Data/Palladin.Data.EntityFramework/DBGlobals.cs
+++ src/Data/Palladin.Data.EntityFramework/DBGlobals.cs
@@ -4,6 +4,6 @@
     {
         public const string SChemaName = "PalladinReport";
         //public const string DbConnection = @"Server=45.57.179.85;Database=PallRepo;User Id=palladin;Password='eSecurity@1qaz2wsx'";
-        public const string DbConnection = @"Server=.;Database=PallRepo;User Id=sa;Password='MyComplexPass!234;'";
+        public const string DbConnection = @"Server=dbserver;Database=PallRepo;User Id=sa;Password='MyComplexPass!234'";
     }
 }
